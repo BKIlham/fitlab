4c3981d50153f831fbd5418c5f3360d0c5b238d8
Bintang Kusuma Ilham <76105273+BKIlham@users.noreply.github.com>
2025-12-22 08:35:50 +0700

    fix(FormSection): Revise registration confirmation and details
    Updated registration message to clarify agreement and winner announcement process.

--- components/FormSection.tsx
+++ components/FormSection.tsx
@@ -94,7 +94,8 @@ export default function FormSection() {
             <span className="text-[#FDBA19]">DISINI</span>
           </h2>
           <p className="text-white/90 text-lg font-medium leading-relaxed">
-            Isi data diri dan bawa pulang hadiahnya! Dipilih 3 pemenang setiap hari. Pemenang akan diumumkan di Instagram <Link href="https://www.instagram.com/fitlab.indonesia/" target='_blank' rel='noopener noreferrer' className='text-[#FDBA19] font-bold'>@fitlab.indonesia</Link> dan dihubungi melalui Whatsapp <br /> <strong className="underline decoration-[#FDBA19] decoration-4 underline-offset-4">100% GRATIS.</strong>
+            Dengan mengisi data ini, Anda menyetujui untuk mengikuti undian Fitlab.
+Setiap hari akan dipilih 3 pemenang yang akan diumumkan melalui Instagram dan dihubungi melalui WhatsApp.
           </p>
         </div>
 
@@ -107,8 +108,8 @@ export default function FormSection() {
                   🎉
                 </div>
                 <div>
-                  <h3 className="text-3xl font-black text-[#0B897A] mb-2">Permintaan Diterima!</h3>
-                  <p className="text-slate-500 font-medium">Semoga Kamu Beruntung!</p>
+                  <h3 className="text-3xl font-black text-[#0B897A] mb-2">Kamu Sudah Terdaftar</h3>
+                  <p className="text-slate-500 font-medium">Semoga Beruntung!</p>
                 </div>
                 <button onClick={() => setStatus('idle')} className="text-[#12B4A0] font-black underline hover:text-[#0B897A]">Kirim data lain</button>
               </div>
